29aa739fbcb0cf2a569a682c38681c5d450347a5
jem-donato <jemdonato@gmail.com>
2025-04-11 01:45:53 +0800

    removed .next file, added gitignore, eslint files, updated next config and package.json

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'react-hooks'],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
